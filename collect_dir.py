ee46e6e1630d894d0a02fd1bb9bd2ef133ed30db
NeoShu <random.shu@gmail.com>
2024-07-08 23:55:07 +0800

    Made some improvements on file collect_dir.py Now collect_dir.py can contain every folder with or without subfiles. However, the method is far from being concise and elegance.

--- collect_dir.py
+++ collect_dir.py
@@ -4,16 +4,32 @@
 """
 from pathlib import Path
 
+def are_all_file(path):
+    judge_list = list(path.iterdir())
+    for item in judge_list:
+        if not item.is_file():
+            return False
+    return True
+
 def collect(path):
     criteria = list(path.iterdir())
     if len(criteria) == 0:
         return path
+    elif are_all_file(path):
+        return path
     else:
         collected_dirs = []
-        # criteria = list(path.iterdir())
         for each_dir in criteria:
-            collected_dirs.append(collect(each_dir))
+            if each_dir.is_dir():
+                collected_dirs.append(collect(each_dir))
         return collected_dirs
 
 path = Path("new_dir")
-print(collect(path))
+result = collect(path)
+corrected_result = []
+for item in result:
+    if type(item) != list:
+        corrected_result.append(item)
+    else:
+        corrected_result.extend(item)
+print(corrected_result)
